5f9df4600a9b84af3612737cd653b13e5bb7a313
Carly <carlyne.lapierre@gmail.com>
2019-11-05 18:14:29 +0100

    success js

--- Wireframe/script.js
+++ Wireframe/script.js
@@ -10,8 +10,9 @@ for (i = 0; i < cards.length; i++) {
             card.style.width = '100%';
             card.style.margin = '0';
             card.style.height = '100vh';
-            card.style.order = '-1';
             card.style.border = '4px solid #000';
+            card.style.position = 'fixed';
+            card.style.zIndex = '100';
 
             full = true;
 
@@ -19,8 +20,9 @@ for (i = 0; i < cards.length; i++) {
             card.style.width = '';
             card.style.margin = '';
             card.style.height = '';
-            card.style.order = '';
             card.style.border = '';
+            card.style.position = '';
+            card.style.zIndex = '';
 
             full = false;
         }
