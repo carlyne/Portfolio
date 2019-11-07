634ac5808c42600e75a61213d5f0828ca6219f3b
Carly <carlyne.lapierre@gmail.com>
2019-11-07 15:10:10 +0100

    amelioration graphiques générales

--- Wireframe/script.js
+++ Wireframe/script.js
@@ -1,5 +1,23 @@
 let cards = document.querySelectorAll('.card');
+let encarts = document.querySelectorAll('.encart');
 let full = false;
+
+function displayEncart(displayStatut, transformStatut) {
+    for(i = 0; i < encarts.length; i++) {
+        let encart = encarts[i];
+                
+        encart.style.display = displayStatut;
+    }
+}
+
+function sizeProject(classProject) {
+    if(full === false) {
+        document.querySelector(classProject).style.backgroundSize = 'contain';
+    } else if (full === true){
+        document.querySelector(classProject).style.backgroundSize = 'cover';
+    }
+
+}
 
 for (i = 0; i < cards.length; i++) {
     let card = cards[i];
@@ -7,6 +25,7 @@ for (i = 0; i < cards.length; i++) {
     card.addEventListener('click', event => {
 
         if (full === false) {
+            sizeProject('.project01');
             card.style.width = '100%';
             card.style.margin = '0';
             card.style.height = '100vh';
@@ -17,11 +36,12 @@ for (i = 0; i < cards.length; i++) {
             card.style.zIndex = '100';
             card.style.transform = 'translateX(1%) scale(1.01, 1.01)';
             
-            document.querySelector('.encart').style.display = 'block';
+            displayEncart('block');
             
             full = true;
 
         } else if (full === true) {
+            sizeProject('.project01');
             card.style.width = '';
             card.style.margin = '';
             card.style.height = '';
@@ -30,7 +50,7 @@ for (i = 0; i < cards.length; i++) {
             card.style.zIndex = '0';
             card.style.transform = 'translateX(0) scale(1, 1)';
             
-            document.querySelector('.encart').style.display = 'none';
+            displayEncart('none');
             
             full = false;
         }
