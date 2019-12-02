b85bb250b806df73f17de3419171055f86a7eef1
Carly <carlyne.lapierre@gmail.com>
2019-12-02 14:08:32 +0100

    hover and click gallery

--- Wireframe/script.js
+++ Wireframe/script.js
@@ -1,17 +1,14 @@
 const cards = document.getElementById("board-session").getElementsByTagName("img");
 
 
-
-
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
 
     card.addEventListener('click', e => {
-
         if (e.target.classList.contains('full-size')) {
-            e.target.classList.remove('full-size');
+            e.target.classList.remove('full-size', 'no-hover');
         } else {
-            e.target.classList.add('full-size');
+            e.target.classList.add('full-size', 'no-hover'); 
         }
     })
 }
