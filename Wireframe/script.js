e2f69f7411dd65ceeb5e783bfe99bf9dca899c1c
Carly <carlyne.lapierre@gmail.com>
2019-12-02 13:38:35 +0100

    gallerie fonctionnelle

---
+++ Wireframe/script.js
@@ -0,0 +1,17 @@
+const cards = document.getElementById("board-session").getElementsByTagName("img");
+
+
+
+
+for (let i = 0; i < cards.length; i++) {
+    let card = cards[i];
+
+    card.addEventListener('click', e => {
+
+        if (e.target.classList.contains('full-size')) {
+            e.target.classList.remove('full-size');
+        } else {
+            e.target.classList.add('full-size');
+        }
+    })
+}
