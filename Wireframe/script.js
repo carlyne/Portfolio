42b90e4417d228240179a594b3da3b0f191c0f35
Carly <carlyne.lapierre@gmail.com>
2019-12-03 15:19:09 +0100

    automatique text

--- Wireframe/script.js
+++ Wireframe/script.js
@@ -1,12 +1,25 @@
 const cards = document.getElementById("board-session").getElementsByTagName("img");
-
 const info = document.getElementById("info");
+const gallery = document.getElementById('gallery');
+const infoText = document.querySelector('#info p');
+
+const descr = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.", "blabla"];
+
 
 
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
+    let description = "";
 
     card.addEventListener('click', e => {
+
+        for (let j = 0; j < descr.length; j++) {
+            let currentDescr = descr[j];
+            if (i === j) {
+                description = currentDescr;
+            }
+        }
+
         if (e.target.classList.contains('full-size')) {
             e.target.classList.remove('full-size', 'no-hover');
 
@@ -15,6 +28,7 @@ for (let i = 0; i < cards.length; i++) {
         } else {
             e.target.classList.add('full-size', 'no-hover');
 
+            infoText.innerHTML = description;
             info.style.display = "block";
             info.classList.add('animation');
         }
