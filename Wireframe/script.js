93f747758fd1e75224ba7817a5e45c1b66eabf59
Carly <carlyne.lapierre@gmail.com>
2019-12-03 13:54:31 +0100

    animation

--- Wireframe/script.js
+++ Wireframe/script.js
@@ -1,5 +1,7 @@
 const cards = document.getElementById("board-session").getElementsByTagName("img");
 
+const info = document.getElementById("info");
+
 
 for (let i = 0; i < cards.length; i++) {
     let card = cards[i];
@@ -7,8 +9,14 @@ for (let i = 0; i < cards.length; i++) {
     card.addEventListener('click', e => {
         if (e.target.classList.contains('full-size')) {
             e.target.classList.remove('full-size', 'no-hover');
+
+            info.style.display = "none";
+            info.classList.remove('animation');
         } else {
-            e.target.classList.add('full-size', 'no-hover'); 
+            e.target.classList.add('full-size', 'no-hover');
+
+            info.style.display = "block";
+            info.classList.add('animation');
         }
     })
 }
